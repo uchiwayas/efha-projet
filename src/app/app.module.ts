5d44f58038fa6ad58849694068443fe93a882192
uchiwayas <yassine.tahri@outlook.fr>
2020-02-10 20:17:26 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import {RouterModule} from '@angular/router';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
-import { Page3Component } from './page3/page3.component';
-import { Page4Component } from './page4/page4.component';
+import { Page1Component } from './handicap-visuel/page1.component';
+import { Page2Component } from './handicap-moteur/page2.component';
+import { Page3Component } from './page-contact/page3.component';
+import { Page4Component } from './plan-du-site/page4.component';
 import {FormsModule} from "@angular/forms";
-import { Page5Component } from './page5/page5.component';
+import { Page5Component } from './handicap-auditif/page5.component';
 
 @NgModule({
   declarations: [
@@ -28,11 +28,11 @@ import { Page5Component } from './page5/page5.component';
     RouterModule.forRoot([
       {path: '', redirectTo: '/index', pathMatch: 'full'},
       {path: 'index', component: MainMenuComponent},
-      {path: 'page1', component: Page1Component},
-      {path: 'page2', component: Page2Component},
-      {path: 'page3', component: Page3Component},
-      {path: 'page4', component: Page4Component},
-      {path: 'page5', component: Page5Component},
+      {path: 'handicap-visuel', component: Page1Component},
+      {path: 'handicap-moteur', component: Page2Component},
+      {path: 'page-contact', component: Page3Component},
+      {path: 'plan-du-site', component: Page4Component},
+      {path: 'handicap-auditif', component: Page5Component},
       {path: '**', redirectTo: '/index', pathMatch: 'full'}
     ]),
     FormsModule
